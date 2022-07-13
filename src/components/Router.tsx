663fd251641ebff728029a8aa3299582e3e4ca4b
isk-bokang <bo.kang@iskra.world>
2022-07-13 18:52:06 +0900

    feat: add service router

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ChainsRouter } from "./contracts/ChainDiv";
 import { ContractListDiv, ContractsRouter } from "./contracts/ContractsDiv";
 import { DeployedContractsRouter } from "./contracts/DeployedContract";
+import { ServicesRouter } from "./contracts/ServicesDiv";
 
 export function Router() {
 
@@ -13,6 +14,7 @@ export function Router() {
                     <Route path="/contracts/*" element={<ContractsRouter />} />
                     <Route path="/chains/*" element={<ChainsRouter />} />
                     <Route path="/deployed/*" element={<DeployedContractsRouter/>}/>
+                    <Route path="/services/*" element={<ServicesRouter/>}/>
                 </Routes>
             </BrowserRouter>
         </>
